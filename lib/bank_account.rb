a22114404e66a45fb85a2ca82786fa48b72c5fc4
Tyler Young <tyoung900@gmail.com>
2020-12-10 22:09:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,10 @@
 class BankAccount
-  attr_reader :name
+  attr_reader :name, :balance, :status
 
-  def initialize(name)
+  def initialize(name, balance = '1000', status = "open")
     @name = name
+    @balance = balance
+    @status = status
   end
 
 
